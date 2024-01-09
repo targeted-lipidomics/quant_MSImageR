cb7cdf1c3a64b0c64a652790289a97c865d81a03
Matthew Smith <matthew.smith@ki.se>
2024-01-09 14:50:50 +0100

    update data

--- R/extdata.R
+++ R/extdata.R
@@ -1,10 +1,14 @@
-#' Dataset foo
+#' Dataset example_MRM_raw
 #'
-#' This is a dataset containing stuff.
+#' This is a dataset containing Waters MRM data of SphingoMyelin 16:00 in Guinea pig lung tissue
 #'
-#' @name raw_datasetName
+#' @name example_MRM_raw
 #'
-#' @section Analyte1.txt:
+#' @section example_MRM_raw.raw/imaging/Analyte 1.txt:
 #'
-#' This data is used in thisFunction().
+#' This data is used in read_mrm().
 NULL
+
+
+#a = system.file('extdata','Analyte1.txt', package = 'quantMSImageR')
+#file.exists(a)
