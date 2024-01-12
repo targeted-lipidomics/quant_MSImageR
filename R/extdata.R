4375441a607f3cffe45367d7b51069b8b1cbe850
Matthew Smith <matthew.smith@ki.se>
2024-01-12 14:04:28 +0100

    update test data

--- R/extdata.R
+++ R/extdata.R
@@ -1,10 +1,22 @@
-#' Dataset example_MRM_raw
+#' Dataset tissue_MRM_data
 #'
 #' This is a dataset containing Waters MRM data of SphingoMyelin 16:00 in Guinea pig lung tissue.
 #'
-#' @name example_MRM_raw
+#' @name tissue_MRM_data
 #'
-#' @section example_MRM_raw.raw/imaging/Analyte 1.txt:
+#' @section tissue_MRM_data.raw/imaging/Analyte 1.txt:
+#'
+#' This data is used in read_mrm().
+NULL
+
+
+#' Dataset cal_MRM_data
+#'
+#' This is a dataset containing Waters MRM data of SphingoMyelin 16:00 in Guinea pig lung tissue.
+#'
+#' @name cal_MRM_data
+#'
+#' @section cal_MRM_data.raw/imaging/Analyte 1.txt:
 #'
 #' This data is used in read_mrm().
 NULL
