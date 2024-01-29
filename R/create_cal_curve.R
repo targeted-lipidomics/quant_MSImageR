a21c8444abf1a0eb37899db2ee2c9ce9cd4a1139
Matthew Smith <matthew.smith@ki.se>
2024-01-29 22:10:36 +0100

    update cal curve info

--- R/create_cal_curve.R
+++ R/create_cal_curve.R
@@ -12,7 +12,7 @@ setGeneric("create_cal_curve", function(MSIobject, ...) standardGeneric("create_
 #' @include setClasses.R
 #'
 #' @param response_matrix matrix of average ng/pixel of m/z (rows = m/z and cols = cal level)
-#' @param cal_type string of approach to generate claibration curve - 'std_addition' is default
+#' @param cal_type string of approach to generate claibration curve - 'std_addition' if standards are on tissue and 'cal' if direct onto glass slide.
 #' @return MSIobject with slots updated for i) cal_list - List of linear models for each m/z (response v concentration, where concentration is ng/pixel) and ii) r2 values for each calibration iii) calibration metadata
 #'
 #' @export
