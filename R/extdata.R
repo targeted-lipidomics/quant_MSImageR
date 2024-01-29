6230dc7e1cc8dd1426ead96ada8324eb0db59d39
Matthew Smith <matthew.smith@ki.se>
2024-01-29 21:52:42 +0100

    update example data

--- R/extdata.R
+++ R/extdata.R
@@ -30,5 +30,53 @@ NULL
 #'
 #' @section ion_library.txt:
 #'
-#' This data is not yet used.
+#' This data is used in read_mrm().
+NULL
+
+
+#' Dataset cal_rois
+#'
+#' This is a table containing a logical data about caliobration levels pixels are associated with.
+#'
+#' @name cal_rois
+#'
+#' @section cal_rois.csv:
+#'
+#' This data is used to identify calibration spots.
+NULL
+
+
+#' Dataset calibration_metadata
+#'
+#' This is a table containing information about concentrations in the calibration spots.
+#'
+#' @name calibration_metadata
+#'
+#' @section calibration_metadata.csv:
+#'
+#' This data is used to create calibration curves in create_cal_curve().
+NULL
+
+
+#' Dataset tissue_pixels
+#'
+#' This is a table containing a logical data about whether pixel is part of a calibration level.
+#'
+#' @name tissue_pixels
+#'
+#' @section tissue_pixels.csv:
+#'
+#' This data is used to identify tissue pixels.
+NULL
+
+
+#' Dataset tissue_rois
+#'
+#' This is a table containing a logical data about which tissue types pixels are associated with.
+#'
+#' @name tissue_rois
+#'
+#' @section tissue_rois.csv:
+#'
+#' This data is used to identify and label tissue types.
 NULL
