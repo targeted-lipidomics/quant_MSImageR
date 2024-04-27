7d836b2da714fd4090888ddd566d209b69903a4d
Matthew Smith <matthew.smith@ki.se>
2024-04-27 20:18:49 +0200

    add histogram scale

--- R/imageR.R
+++ R/imageR.R
@@ -19,73 +19,83 @@ setGeneric("imageR", function(MSIobject, ...) standardGeneric("imageR"))
 #' @param sample_lab character header from pData(MSIobject) to label images (defaults to "sample_ID")
 #' @param pixels character from pData(MSIobject)$sample_type to take pixels to plot (defaults to NA)
 #' @param overlay whether to overlay features (not yet implemented!)
+#' @param feat_ind Index of feature from fData(MSIobject) to image
 #' @return ggplot object
 #'
 #' @export
 setMethod("imageR", "quant_MSImagingExperiment",
           function(MSIobject, value = "response %", scale = "suppress", threshold = 1,
-                   sample_lab = "sample_ID", pixels = NA, percentile=99.0, overlay = F){
+                   sample_lab = "sample_ID", pixels = NA, percentile=99.0, overlay = F,
+                   feat_ind = 1){
 
-            MSIobject = as(MSIobject, "quant_MSImagingExperiment")
+            MSIobject = as(MSIobject[feat_ind, ], "quant_MSImagingExperiment")
 
             if(!is.na(pixels)){
               MSIobject = MSIobject[, which(pData(MSIobject)$sample_type == pixels)]
             }
 
-            image_df = tibble(x=numeric(), y=numeric(), response = numeric(), sample=character())
-
-            # Iterate over features in study
-            for(mz_ind in 1:nrow(fData(MSIobject))){
-
-              # Generate image data matrix
-              temp_df = tibble(x = pData(MSIobject)@coord@listData[["x"]],
-                                y = pData(MSIobject)@coord@listData[["y"]],
-                                response = spectra(MSIobject)[mz_ind, ],
-                                sample = pData(MSIobject)[[sample_lab]],
-                                feature = fData(MSIobject)$name[mz_ind])
+            # Generate image data matrix
+            image_df = tibble(x = pData(MSIobject)@coord@listData[["x"]],
+                              y = pData(MSIobject)@coord@listData[["y"]],
+                              response = as.numeric(spectra(MSIobject)),
+                              sample = pData(MSIobject)[[sample_lab]],
+                              feature = fData(MSIobject)$name)
 
 
-              if(scale == "sqrt"){
-                temp_df = mutate(temp_df, response = sqrt(response))
-              }
-              if(scale == "suppress"){
-                vals = temp_df$response
-                cutoff <- quantile(vals, percentile /100, na.rm=TRUE)
-                if (cutoff > min(vals, na.rm=TRUE)){
-                  vals[vals > cutoff] <- cutoff
-                }
-
-                temp_df = mutate(temp_df, response = vals)
-              }
-              if(scale == "histogram"){
-                print("not implemented")
-                #-	https://rdrr.io/bioc/Cardinal/src/R/DIP.R
+            if(scale == "sqrt"){
+              image_df = mutate(image_df, response = sqrt(response))
+            }
+            if(scale == "suppress"){
+              vals = image_df$response
+              max_val = quantile(vals, percentile /100, na.rm=TRUE)
+              if (max_val > min(vals, na.rm=TRUE)){
+                vals[vals > max_val] = max_val
               }
 
-              if(overlay ==T){
+              image_df = mutate(image_df, response = vals)
+            }
+            if(scale == "histogram"){
 
-              }
+              # Cardinal implementation	https://rdrr.io/bioc/Cardinal/src/R/DIP.R
 
+              vals = image_df$response
 
-              # Remove x% of values - threshold
-              vals = temp_df$response
-              cutoff <- quantile(vals, threshold /100, na.rm=TRUE)
-              if (cutoff < max(vals, na.rm=TRUE)){
-                vals[vals < cutoff] <- 0
-              }
-              temp_df = mutate(temp_df, response = vals)
+              unique_percentiles = unique(quantile(vals, seq(from=0, to=1, length.out=100), na.rm=TRUE))
+              if (length(unique_percentiles) < 5){
+                return("Range of intensity values too narrow for histogram normalization.") }
 
+              vals_interval = cut(vals, unique_percentiles, include.lowest=TRUE)
+              vals_new = as.numeric(vals_interval) / length(levels(vals_interval))
 
-              # Set NA and negative values to 0 for plotting!
-              temp_df = temp_df %>%
-                mutate(response = ifelse(is.na(response), 0, response)) %>%
-                mutate(response = ifelse(response < 0, 0, response))
+              scale = mean(vals, na.rm=TRUE) / mean(vals_new, na.rm=TRUE) # So mean value same as prior to scaling
+              vals_new = scale * vals_new
 
-              image_df = rbind(image_df, temp_df)
+              image_df = mutate(image_df, response = vals_new)
 
             }
 
-            p = ggplot(data=temp_df,aes(x=x,y=-y,fill=response))+
+            if(!is.na(threshold)){
+              # Remove x% of values - threshold
+              vals = image_df$response
+              min_val = quantile(vals, threshold /100, na.rm=TRUE)
+              if (min_val < max(vals, na.rm=TRUE)){
+                vals[vals < min_val] = 0
+              }
+              image_df = mutate(image_df, response = vals)
+            }
+
+            if(overlay ==T){
+              return("Overlay functionality not added yet. Set to FALSE and rerun.")
+            }
+
+            # Set NA and negative values to 0 for plotting!
+            image_df = image_df %>%
+              mutate(response = ifelse(is.na(response), 0, response)) %>%
+              mutate(response = ifelse(response < 0, 0, response))
+
+            image_df = rbind(image_df, image_df)
+
+            p = ggplot(data=image_df,aes(x=x,y=-y,fill=response))+
               geom_tile() +
               theme_minimal() +
               theme(axis.title = element_blank(),
@@ -97,6 +107,8 @@ setMethod("imageR", "quant_MSImagingExperiment",
               labs(fill=value) +
               facet_grid(sample~feature)
 
+            p
+
             return(p)
 
-          })
+        })
