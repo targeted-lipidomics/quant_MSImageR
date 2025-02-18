5e84d99a4376848fd3d24d7569d38f3589ec9a85
Matthew Smith <matthew.smith@ki.se>
2025-02-18 17:32:55 +0100

    Fix warning message

--- R/read_mrm_combined.R
+++ R/read_mrm_combined.R
@@ -9,7 +9,6 @@ require(purrr)
 #' @param name Name of Waters raw imaging file (including the imaging/Analyte 1.txt file after processing)
 #' @param folder Location of folder with raw imaging data in
 #' @param lib_ion_path Full path to library of transitions for targeted MSI experiments. Must include headers: 'transition_id',	'precursor_mz',	'product_mz',	'collision_eV',	'cone_V',	'Polarity',	'Type'. Where 'Type' is "Analyte" for analytes in tissue and "IS" for internal standards.
-#' @param polarity String indicating whether data was collected in 'Positive' or 'Negative' polarity.
 #'
 #' @return MSIobject with slots updated for i) matrix of average ng/pixel of m/z (rows = m/z and cols = cal level) in tissue ROIs ii) sample/ROI metadata
 #'
